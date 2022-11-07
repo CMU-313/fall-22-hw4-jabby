18221e4e4f8f1b8da950fc0cd7172a1814a408fe
Brandon Wang <brandonwang2000@gmail.com>
2022-11-07 17:52:09 -0500

    reduce model to 4 features and updated query format

--- app/handlers/routes.py
+++ app/handlers/routes.py
@@ -19,14 +19,26 @@ def configure_routes(app):
     @app.route('/predict')
     def predict():
         #use entries from the query string here but could also use json
-        age = request.args.get('age')
+        failures = request.args.get('failures')
         absences = request.args.get('absences')
-        health = request.args.get('health')
-        data = [[age], [health], [absences]]
+        G1 = request.args.get('G1')
+        G2 = request.args.get('G2')
+        
+        # TODO: check that all 4 features are provided (not None)
+
+
+        # TODO: check that all 4 features are the proper type (ints or can be parsed to ints)
+
+
+        # TODO: check that all 4 features are in the right ranges
+
+
+        # pass features as a query into the ML model
         query_df = pd.DataFrame({
-            'age': pd.Series(age),
-            'health': pd.Series(health),
-            'absences': pd.Series(absences)
+            'failures': pd.Series(failures),
+            'absences': pd.Series(absences),
+            'G1': pd.Series(G1),
+            'G2': pd.Series(G2)
         })
         query = pd.get_dummies(query_df)
         prediction = clf.predict(query)
