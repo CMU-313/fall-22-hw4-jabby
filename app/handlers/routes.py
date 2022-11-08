a6ad6570f726b8e45054bd397f1700ca9b2e8ffa
Brandon Wang <brandonwang2000@gmail.com>
2022-11-08 18:15:23 -0500

    quick fitx: 0 ≤ failures ≤ 3

--- app/handlers/routes.py
+++ app/handlers/routes.py
@@ -49,7 +49,7 @@ def configure_routes(app):
             # must be in the right ranges
             return val >= vmin and val <= vmax
 
-        if not (check422(failures, 1, 4) and check422(absences, 0, 93) \
+        if not (check422(failures, 0, 3) and check422(absences, 0, 93) \
             and check422(G1, 0, 20) and check422(G2, 0, 20)):
             return ERROR_422, 422
 
