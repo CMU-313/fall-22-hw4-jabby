c126e9f9bbcc9685a80f3dabbfc9ef9b23f1fc97
Jake Zych <zych.jake@gmail.com>
2022-11-08 16:16:58 -0500

    implement the predict endpoint

--- app/handlers/routes.py
+++ app/handlers/routes.py
@@ -10,13 +10,16 @@ def configure_routes(app):
     this_dir = os.path.dirname(__file__)
     model_path = os.path.join(this_dir, "model.pkl")
     clf = joblib.load(model_path)
-
+    
     @app.route('/')
     def hello():
-        return "try the predict route it is great!"
+        return "to interact with this API, use the predict route", 200 
 
     @app.route('/predict')
     def predict():
+        ERROR_422 = jsonify({"error": "Query values do not satisfy accepted data ranges"})
+        ERROR_400 = jsonify({"error": "Malformed request"})
+        
         #use entries from the query string here but could also use json
         failures = request.args.get('failures')
         absences = request.args.get('absences')
@@ -24,17 +27,17 @@ def configure_routes(app):
         G2 = request.args.get('G2')
         
         # status=400: check that all 4 features are provided (not None)
-        if failures==None or absences==None or G1==None or G2==None:
-            return 'Must provide failures, absences, G1, and G2', 400
+        if failures == None or absences == None or G1 == None or G2 == None:
+            return ERROR_400, 400
             
 
         # status=422: check values are okay
         def check422(var, vmin, vmax):
             # must be int or can be parsed to int
             val = None
-            if type(var)==int:
+            if type(var) == int:
                 val = var
-            elif type(var)==str and var.isdigit():
+            elif type(var) == str and var.isdigit():
                 val = int(var)
             else:
                 return False
@@ -44,11 +47,11 @@ def configure_routes(app):
                 return False
             
             # must be in the right ranges
-            return val>=vmin and val<=vmax
+            return val >= vmin and val <= vmax
 
-        if not (check422(failures, 0, 3) and check422(absences, 0, 93) \
+        if not (check422(failures, 1, 4) and check422(absences, 0, 93) \
             and check422(G1, 0, 20) and check422(G2, 0, 20)):
-            return 'values are not integers', 422
+            return ERROR_422, 422
 
 
         # pass features as a query into the ML model
@@ -60,4 +63,5 @@ def configure_routes(app):
         })
         query = pd.get_dummies(query_df)
         prediction = clf.predict(query)
-        return jsonify(np.asscalar(prediction))
+        result = {"high_quality": np.ndarray.item(prediction) == 1}
+        return jsonify(result), 200
